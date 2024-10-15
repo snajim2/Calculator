b31d91017b9f552e33b9c7a79c72e18b59557250
Sabien Najim <90353428+snajim2@users.noreply.github.com>
2024-10-15 09:38:21 -0700

    Inital Commit

---
+++ Calculator/src/main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+    }
+}
